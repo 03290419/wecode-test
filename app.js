57d879766b77fedb02850127ba3df2a69a225727
hyunjin <alfma123@naver.com>
2023-09-07 20:47:54 +0900

    Feat: port 세팅 및 server listen

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ class App {
   constructor() {
     this.app = express();
     this.setMiddleware();
+    this.setPort();
+  }
+  setPort() {
+    this.app.set('port', process.env.PORT || 8000);
   }
   setMiddleware() {
     this.app.use(cors());
@@ -18,3 +22,8 @@ class App {
     this.app.use(express.urlencoded({ extended: true }));
   }
 }
+
+const app = new App().app;
+app.listen(app.get('port'), () => {
+  console.log(`Server is listening 8000 http://localhost:${app.get('port')}`);
+});
